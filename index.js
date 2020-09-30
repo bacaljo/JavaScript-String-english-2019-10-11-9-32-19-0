69cec66cfd4115e606b665fba71fa3e06bb4fc39
John Emmanuel Bacalla <john.emmanuel.bacalla@oocl.com>
2020-09-30 17:45:28 +0800

    Apply necessary changes to display expected output

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 // Complete the following procedure, the 'name' variable in all letters to uppercase, output: ' 'HELLO' '.
 var name = 'hello';
-// TODO write your code here...
+console.log("' '" + name.toUpperCase() + "' '");
 
 
 // Complete the following procedure, capitalizing all the words in the variable sentence and printing: 'Good Afternoon, Mr Mike.'
 var sentence = 'good afternoon, mr mike.';
-//TODO write your code here...
+var words = sentence.split(' ');
+var titleCasedWords = [];
+words.forEach((word) => {
+    let titleCasedWord = word.substr(0, 1).toUpperCase() + word.substr(1)
+    titleCasedWords.push(titleCasedWord)
+})
+console.log(titleCasedWords.join(' '));
 
 
 // Complete the following procedure to extract the amount of money variable, output: 20.
 var money = '￥20';
-//TODO write your code here...
+var amount = money.substr(1);
+console.log(amount);
